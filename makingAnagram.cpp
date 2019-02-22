c5f701d5e013dcace064dac767220691ea9213f2
Bhavya Arora <helloworld09071994@gmail.com>
2019-02-23 00:17:39 +0530

    Alternating Characters

---
+++ makingAnagram.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the makeAnagram function below.
+int makeAnagram(string a, string b) {
+    
+return 0;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string a;
+    getline(cin, a);
+
+    string b;
+    getline(cin, b);
+
+    int res = makeAnagram(a, b);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+

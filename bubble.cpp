2cb3886cd8ca0d84e8a2cdde315b64c51bab3500
Bhavya Arora <helloworld09071994@gmail.com>
2019-03-26 00:01:56 +0530

    1

---
+++ bubble.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a, b;
+   int c;
+   float f;
+   a = 10;
+   b = 20;
+   c = a + b;
+   cout << c << endl ;
+
+   f = 70.0/3.0;
+   cout << f << endl ;}
+//	double res;
+//	res=b+d;
+//	cout<<res;}
+	
+//	int a[]={2,3,4,1};
+//	int swap;
+//	int n=4;
+//	for(int i=0;i<n-1;i++){
+//		for(int j=0;j<n-i-1;j++)
+//		{
+//			if(a[j]>a[j+1]){
+//			int temp;
+//			temp=a[j];
+//			a[j]=a[j+1];
+//			a[j+1]=temp;
+//			++swap;
+//		 }
+//		}
+//	}
+//	printf("array is");
+//	for(int k=0;k<n;k++){
+//	printf("%d",a[k]);
+//}
+//printf("\n");
+//	printf("%d",swap);
+//}
